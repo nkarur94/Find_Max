8af091091b7e2cfe8e96c52c0f07554dc0f1b87a
nithunkarur <rknithun@gmail.com>
2021-10-31 17:58:26 +0530

    ADDED: Created the object of float type to find the maximum of numbers

--- GenericDemo.java
+++ GenericDemo.java
@@ -3,6 +3,11 @@ package com.assignment13;
 public class GenericDemo {
     public static void main(String[] args) {
         Generics <Integer> findMax=new Generics<>(33, -55, 24);
+        Generics <Float> findMaxF=new Generics<>(3.3F, 4.5F, -3.2F);
+
         findMax.maximumOfNum();
+        findMaxF.maximumOfNum();
+
+
     }
 }
